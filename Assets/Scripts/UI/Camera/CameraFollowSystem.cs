f1be5361a89486a7d193fcb3fb16bf45f5133fe2
lZelie <zelie.leydert@gmail.com>
2025-02-14 14:01:55 +0100

    Changement du calcul de la positionde la caméra et colisions

--- Assets/Scripts/UI/Camera/CameraFollowSystem.cs
+++ Assets/Scripts/UI/Camera/CameraFollowSystem.cs
@@ -1,18 +1,46 @@
+using System;
 using UnityEngine;
 
 public class CameraFollowSystem : MonoBehaviour
 {
-    [SerializeField] public Transform target; // Référence au joueur
-    [SerializeField] private Vector3 offset = new Vector3(0, 70, -30); // Position relative par rapport au joueur
+    [SerializeField] public Transform target; // RÃ©fÃ©rence au joueur
+    [SerializeField] private Vector3 offset = new Vector3(0, 70, -30); // Position relative par rapport au joueur // Unused
     [SerializeField] private float smoothSpeed = 5f; // Vitesse de lissage du mouvement
+    [SerializeField] private float horizontalAngle = -90; // Horizontal angle from the player
+    [SerializeField] private float verticalAngle = 56; // Vertical angle from the player
+    [SerializeField] private float distance = 25; // Distance from the player
+    private float initialDistance;
+
+    private void Start()
+    {
+        initialDistance = distance;
+    }
 
     private void LateUpdate()
     {
         if (target)
         {
-            // Calcul de la position cible de la caméra
-            Vector3 targetPosition = target.position + offset;
+            // Calcul de la position cible de la camÃ©ra
+            distance = initialDistance;
+            var x = (float) Math.Cos(horizontalAngle * Mathf.Deg2Rad) * Mathf.Cos(verticalAngle * Mathf.Deg2Rad);
+            var y = (float) Math.Sin(verticalAngle * Mathf.Deg2Rad);
+            var z = (float) Math.Sin(horizontalAngle * Mathf.Deg2Rad) * Mathf.Cos(verticalAngle * Mathf.Deg2Rad);
+            var direction = new Vector3(x, y, z).normalized;
+            
+            Vector3 targetPosition = target.position + direction * distance + 1.0f * Vector3.up;
 
+            // Raycast pour detecter si un objet de l'environement bloque la vue du joueur
+            var ray = new Ray(targetPosition + direction * 2.0f, -direction);
+            Physics.Raycast(ray, out var hit, distance + 1.0f);
+            while (hit.collider is not null && hit.collider.gameObject.layer == LayerMask.GetMask("Environment") && distance > 0.05f)
+            {
+                distance -= 0.01f;
+                targetPosition = target.position + direction * distance + 1.0f * Vector3.up;
+
+                ray = new Ray(targetPosition + direction * 2.0f, -direction);
+                Physics.Raycast(ray, out hit, distance + 1.0f);
+            }
+                
             // Interpolation pour un mouvement fluide (facultatif)
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
         }
