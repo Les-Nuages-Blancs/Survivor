82b772ceafe77cccc42df551f996aeb0b3c27c3f
lZelie <zelie.leydert@gmail.com>
2025-02-14 15:41:44 +0100

    Start death

---
+++ Assets/Scripts/Entity/EntitySystems/PlayerDeathSystem.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerDeathSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject deathCanvasPrefab;
+    [SerializeField] private int respawnCooldownSeconds = 15;
+    [SerializeField] private HealthSystem healthSystem;
+    private MonoBehaviour camMono;     
+    
+    private void Start()
+    {
+        camMono = Camera.main?.GetComponent<MonoBehaviour>();
+    }
+
+    public void KillPlayer()
+    {
+        camMono.StartCoroutine(DeathCooldown());
+    }
+
+    private IEnumerator DeathCooldown()
+    {
+        // TODO: Despawn player
+        healthSystem.CurrentHealth = healthSystem.MaxHealth;
+        var canvas = Instantiate(deathCanvasPrefab, transform.position, Quaternion.identity);
+        var deathText = canvas.transform.Find("Respawn Timer").GetComponent<TextMeshProUGUI>();
+        for (var i = 0; i < respawnCooldownSeconds; i++)
+        {
+            deathText.text = "Respawn in: " + (respawnCooldownSeconds - i - 1);
+            yield return new WaitForSeconds(1f);
+        }
+        Destroy(canvas);
+        // TODO: Spawn player
+    }
+}
