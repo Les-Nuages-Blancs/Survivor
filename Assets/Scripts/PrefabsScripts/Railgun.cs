c5875b7a12abe07d78607f6288c527a46dfa727c
Sixelayo <66423373+Sixelayo@users.noreply.github.com>
2025-03-23 16:54:56 +0100

    fix railgun hitboxes

--- Assets/Scripts/PrefabsScripts/Railgun.cs
+++ Assets/Scripts/PrefabsScripts/Railgun.cs
@@ -1,26 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Cinemachine; //for [TagField] (xD)
+using Cinemachine;
+using UnityEngine.UIElements; //for [TagField] (xD)
 public class Railgun : MonoBehaviour
 {    
     [HideInInspector] public bool isReal; //Will the projectil deal damage on collision with ennemy ?
 
     [SerializeField] public float damage = 5f;
-    [SerializeField] public float hitBoxEase = 8f;
+    
     //[SerializeField] private List<GameObject> EffectsPrefab = new List<GameObject>();
     //[SerializeField] private LayerMask includeTriggerLayers;
     [TagField]
     [SerializeField] private List<string> includeDamageTags = new List<string>();
 
+    [Tooltip("warning : must match the visual lenght of railgun vfx !")] //doing it manually would be a waste at runtime. Unless the lenghts can change at runtime then another logic would be responsible for controlling the lenght of the vfx and this dmg dealing script
+    public float length = 18;
+    public float delayBeforeDamage = 0.25f;
+    private float width = 0.5f;
+
 
     // Start is called before the first frame update
     void Start(){
-        Collider[] colliders = Physics.OverlapBox(transform.position, new Vector3(
+        Invoke("DealDamage", delayBeforeDamage);
+    }
+
+    void DealDamage(){
+        Collider[] colliders = Physics.OverlapBox(transform.position + transform.forward * length/2, new Vector3(
             //10, 10, 40
-            transform.localScale.x * hitBoxEase,
-            transform.localScale.y * hitBoxEase,
-            transform.localScale.z / 2 //it's half size of the box
+            width,
+            width+2,
+            length / 2 //it's half size of the box
             ),
             transform.rotation);
         foreach(Collider other in colliders){
@@ -30,7 +40,7 @@ public class Railgun : MonoBehaviour
 
                 if (includeDamageTags.Contains(target.tag))
                 {
-                    //Debug.Log("HIIIIT");
+                    // Debug.Log("railgunHIIIT");
                     float dmg = damage * LevelStateManager.Instance.PlayerDamageMultiplier;
                     
                     //Spawn effect client side : currently no effect on railgun
@@ -46,7 +56,6 @@ public class Railgun : MonoBehaviour
                 }
             }
         }
-        
     }
 
     // Update is called once per frame
